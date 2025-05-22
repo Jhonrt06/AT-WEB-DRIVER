40379b96156720b7b3052616bcfd3325775616e2
Jhon Rodriguez <jhonrt06@gmail.com>
2025-05-22 11:41:47 -0600

    Add new clasess

---
+++ Automation/playwright_constans.py
@@ -0,0 +1,15 @@
+class PlaywrightConstants:
+    """
+    Constants for Playwright automation.
+    """
+
+    # Browser types
+    CHROME = "chrome"
+    FIREFOX = "firefox"
+    WEBKIT = "webkit"
+
+    # Default timeout in milliseconds
+    DEFAULT_TIMEOUT = 30000
+
+    # Default headless mode
+    HEADLESS = True
---
+++ Automation/playwright_utils.py
@@ -0,0 +1,2 @@
+class PlaywrightUtils:
+    pass
